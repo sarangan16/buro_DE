0f1eb509f617961de4ef688e7f8434737af6d575
sarangan16 <mailsarangan@gmail.com>
2026-04-29 09:46:06 +0200

    before i redesign, and rewrite

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
       <div className="bg-white py-24 sm:py-32">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl lg:text-center">
-            <p className="mt-2 text-4xl font-semibold tracking-wide text-pretty sm:text-5xl lg:text-balance text-blue-600 dark:text-blue-500">
+            <p className="mt-2 text-xl font-semibold tracking-wide text-pretty sm:text-5xl lg:text-balance text-blue-600 dark:text-blue-500">
               Streamline Your German Bureaucracy: Everything You Need in One
               Place{" "}
             </p>
